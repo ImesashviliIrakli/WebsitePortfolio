05cdfa63a694de4a118605d8a4d6fc01135c7b65
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2022-07-30 02:12:37 +0400

    Added Certificates as pdfs to the website (page education&certifications)

--- src/components/Certifications/Certifications.component.jsx
+++ src/components/Certifications/Certifications.component.jsx
@@ -1,5 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Certifications.styles.scss";
 import "animate.css/animate.css";
+import {
+  RenderEpamPdf,
+  CSharpBasicPdf,
+  SQLBasicPdf,
+  SQLIntermediatePdf,
+} from "../render-pdfs/render-pdfs.component";
 
 const Certificiations = () => {
   return (
@@ -9,25 +16,95 @@ const Certificiations = () => {
         <div className="timeline ">
           <div className="timeline-card timeline-card-success card shadow-sm">
             <div className="card-body">
-              <div className="h5 mb-1">.Net Development training</div>
-              <h6 className="text-muted">Epam Systems</h6>
-              <div className="text-muted text-small mb-2">MAY/2022</div>
+              <div className="row">
+                <div className="col-12 col-md-6">
+                  <div className="h5 mb-1">.Net Development training</div>
+                  <h6 className="text-muted">Epam Systems</h6>
+                  <div className="text-muted text-small mb-2">MAY/2022</div>
+                  <div>
+                    <ul>
+                      <li>
+                        Learned basic, intermediate and advanced topics in C#
+                        and ASP.NET Core.
+                      </li>
+                      <li>
+                        Created several Web Api's and console applications
+                      </li>
+                      <li>
+                        Had to work on many practice tasks and basic algorithms
+                        like sorting.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div className="col-6">
+                  <RenderEpamPdf />
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="timeline-card timeline-card-primary card shadow-sm">
             <div className="card-body">
-              <div className="h5 mb-1">C# (Basic) </div>
-              <h6 className="text-muted">Hacker rank</h6>
-              <div className="text-muted text-small mb-2">JAN/2022</div>
+              <div className="row">
+                <div className="col-12 col-md-6">
+                  <div className="h5 mb-1">C# (Basic) </div>
+                  <h6 className="text-muted">Hacker rank</h6>
+                  <div className="text-muted text-small mb-2">JAN/2022</div>
+                  <a
+                    className="col-6 navigateButton"
+                    href="https://www.hackerrank.com/certificates/1810861fea69"
+                  >
+                    Credential URL
+                  </a>
+                </div>
+
+                <div className="col-6">
+                  <CSharpBasicPdf />
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="timeline-card timeline-card-success card shadow-sm">
             <div className="card-body">
-              <div className="h5 mb-1">SQL (Basic) and SQL (Intermediate) </div>
-              <h6 className="text-muted">Hacker rank</h6>
-              <div className="text-muted text-small mb-2">DEC/2021</div>
+              <div className="row">
+                <div className="col-12 col-md-6">
+                  <div className="h5 mb-1">
+                    SQL (Basic) and SQL (Intermediate){" "}
+                  </div>
+                  <h6 className="text-muted">Hacker rank</h6>
+                  <div className="text-muted text-small mb-2">DEC/2021</div>
+                  <div>
+                    <a
+                      className="col-6navigateButton"
+                      href="https://www.hackerrank.com/certificates/104484e27a90"
+                    >
+                      SQL (Basic) Credential URL
+                    </a>
+                  </div>
+
+                  <div>
+                    <a
+                      className="col-6 navigateButton"
+                      href="https://www.hackerrank.com/certificates/a101071c1473"
+                    >
+                      SQL (Intermediate) Credential URL
+                    </a>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="row">
+                    <div className="col">
+                      <SQLBasicPdf />
+                    </div>
+                    <div className="col">
+                      <SQLIntermediatePdf />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/components/render-pdfs/render-pdfs.component.jsx
@@ -0,0 +1,66 @@
+import { Document, Page, pdfjs } from "react-pdf";
+import EpamCertificate from "./Certificates/Irakli_Imesashvili.pdf";
+import CSharpBasic from "./Certificates/CSharpBasic.pdf";
+import SQLBasic from "./Certificates/SQL-Basic.pdf";
+import SQLIntermediate from "./Certificates/SQL-Intermediate.pdf";
+import "./render-pdfs.styles.scss";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+export const RenderEpamPdf = () => {
+  return (
+    <div className="Epam-container">
+      <Document
+        file={EpamCertificate}
+        onLoadSuccess={() => {
+          console.log("success");
+        }}
+      >
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
+};
+
+export const CSharpBasicPdf = () => {
+  return (
+    <div className="csharp-container">
+      <Document
+        file={CSharpBasic}
+        onLoadSuccess={() => {
+          console.log("success");
+        }}
+      >
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
+};
+
+export const SQLBasicPdf = () => {
+  return (
+    <div className="sql-basic-container">
+      <Document
+        file={SQLBasic}
+        onLoadSuccess={() => {
+          console.log("success");
+        }}
+      >
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
+};
+export const SQLIntermediatePdf = () => {
+  return (
+    <div className="sql-intermediate-container">
+      <Document
+        file={SQLIntermediate}
+        onLoadSuccess={() => {
+          console.log("success");
+        }}
+      >
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
+};
