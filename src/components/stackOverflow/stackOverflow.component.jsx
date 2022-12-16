7b98a9cb1a594e6ecb873328faf5f867cb798210
Irakli_Imesashvili <77686006+ImesashviliIrakli@users.noreply.github.com>
2022-12-17 03:18:22 +0400

    Added answers to stackoverflow page

--- src/components/stackOverflow/stackOverflow.component.jsx
+++ src/components/stackOverflow/stackOverflow.component.jsx
@@ -2,14 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./stackOverflow.component.scss";
 import allLogos from "../images/Logos.js";
 import { React, useEffect, useState } from "react";
+import { BsCheckSquare, BsCheckSquareFill } from "react-icons/bs";
 
 const StackOverflow = () => {
   const [result, setResult] = useState([]);
 
   const getData = async () => {
     const response = await fetch(
-      ""
-      //"https://api.stackexchange.com/2.3/users/15657255/answers?order=desc&sort=activity&site=stackoverflow&filter=!)qOzVaFZMxnuRG2dXa-d"
+      "https://api.stackexchange.com/2.3/users/15657255/answers?order=desc&sort=activity&site=stackoverflow&filter=!LJIdoaPLxUEUAuYs8xa7z5"
     );
 
     const res = await response.json();
@@ -25,20 +25,42 @@ const StackOverflow = () => {
       <div className="row d-flex justify-content-center ">
         {result.map((item, index) => {
           return (
-            <div className="card m-3" key={index} style={{ width: "25rem" }}>
-              <div className="card-header">
+            <div className="card mt-4" key={index} style={{ width: "80%" }}>
+              <div className="stack-header card-header d-flex justify-content-between mt-2">
                 <img src={allLogos.stackLogo} alt="StackOverflow Logo" />
-                <a href={item.link}> Click here to view question</a>
+                <a className="stack-link" href={item.link}>
+                  {" "}
+                  Click here to view question
+                </a>
               </div>
               <div className="card-body">
-                <h5 className="card-title">{item.title}</h5>
+                <div className="question-title card-title">
+                  <h5>{item.title}</h5>
+                </div>
+                <h6>My Answer:</h6>
+                <div
+                  className="card-text"
+                  dangerouslySetInnerHTML={{ __html: item.body }}
+                ></div>
               </div>
-              <div className="card-footer">
-                <img
-                  className="stack-profile-image"
-                  src={item.owner.profile_image}
-                  alt="profile"
-                />
+              <div className="card-footer d-flex justify-content-between">
+                <div>
+                  <img
+                    className="stack-profile-image"
+                    src={item.owner.profile_image}
+                    alt="profile"
+                  />
+                </div>
+
+                {item.is_accepted ? (
+                  <div>
+                    <BsCheckSquareFill color="green" />
+                  </div>
+                ) : (
+                  <div>
+                    <BsCheckSquare color="green" />
+                  </div>
+                )}
               </div>
             </div>
           );
